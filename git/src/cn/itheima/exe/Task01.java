2324eb5d7ad9f030940d166ea2e93f32f0ef6cc1
gg <2231203280@qq.com>
2020-10-18 16:54:40 +0800

    IDEA提交代码v2

--- git/src/cn/itheima/exe/Task01.java
+++ git/src/cn/itheima/exe/Task01.java
@@ -3,5 +3,6 @@ package cn.itheima.exe;
 public class Task01 {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("helloworld");
     }
 }
